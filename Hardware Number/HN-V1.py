34c51d591a80dda937f74127a26e7870cb3be6ee
sheen <760162752@qq.com>
2022-08-10 22:50:49 +0800

    MVP

--- Hardware Number/HN-V1.py
+++ Hardware Number/HN-V1.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 import wmi
+import getpass
+print(getpass.getuser())#获取用户名
+
+cname = getpass.getuser()
+
 c = wmi.WMI()
 cpuid=''
-
+BoardSnid=''
 for cpu in c.Win32_Processor():
         cpuid = cpu.ProcessorId.strip()
 
 print(cpuid)
+for board_id in c.Win32_BaseBoard():
 
+        BoardSnid = board_id.SerialNumber.strip()
+txt = cpuid +'\n' + BoardSnid
 #写入本地
-with open('data.txt','w',encoding='utf-8') as f:
-        f.write(cpuid)
+with open(f'{cname}.txt','w+',encoding='utf-8') as f:
+        f.write(txt)
+
         print('写入成功')
